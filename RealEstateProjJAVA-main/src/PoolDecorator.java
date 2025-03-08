4c72dc514c945b9c082ceea3f9a40be7d2b0ac77
Kenshin Alcazar <kenshinalcazar@gmail.com>
2025-03-08 19:48:50 +0800

    removed the redundant decorators

--- RealEstateProjJAVA-main/src/PoolDecorator.java
+++
@@ -1,4 +0,0 @@
-// This class is now redundant and can be removed.
-// All functionality has been moved to the unified LotDecorator class
-// that handles both feature and status decorations with price constants
-// defined in LotFactory.
--- RealEstateProjJAVA-main/src/ReservedLotDecorator.java
+++
@@ -1,3 +0,0 @@
-// This class is now redundant and can be removed.
-// All functionality has been moved to the unified LotDecorator class
-// that handles both feature and status decorations.
